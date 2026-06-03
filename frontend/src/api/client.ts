cc3c774b1135e36b1c1b22f91b915a0354ae2ffa
Vladislav <70677188+UGamerZ@users.noreply.github.com>
2026-06-03 13:37:44 +0800

    Update client.ts: Undo changes

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: process.env.API_URL || '/api',
+  baseURL: '/api',
   headers: {
     'Content-Type': 'application/json',
   },
