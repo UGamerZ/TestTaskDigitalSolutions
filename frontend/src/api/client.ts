2cb284f2c3be4ad2b2b2c9e100392b589b38a699
Vladislav <70677188+UGamerZ@users.noreply.github.com>
2026-06-03 13:33:35 +0800

    Update client.ts: Add env api url

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const apiClient = axios.create({
-  baseURL: '/api',
+  baseURL: process.env.API_URL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
